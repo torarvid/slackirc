8af8db0ed39e4d83a8efceaad50801df6f0cbf71
Tor Arvid Lund <torarvid@comoyo.com>
2014-09-03 13:17:19 +0200

    Added winston logging

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var config = require('config');
 var express = require('express');
 var bodyParser = require('body-parser');
+var l = require('./lib/log');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/toirc', function(req, res){
-  console.log('Got incoming msg to IRC: ', req.body);
+  l.debug('Got incoming msg to IRC: ', req.body);
   res.status(200).end();
 });
 
 var server = app.listen(config.server.port, function() {
-  console.log('Listening on port ' + config.server.port);
+  l.info('Listening on port ' + config.server.port);
 });
