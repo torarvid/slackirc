b843f62cd256174a6710035334c643658588e53f
Tor Arvid Lund <torarvid@comoyo.com>
2014-09-03 13:59:03 +0200

    Change server to actually try to send messages to IRC

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var config = require('config');
 var express = require('express');
 var bodyParser = require('body-parser');
+var ircdispatcher = require('./lib/ircdispatcher');
+var messageparser = require('./lib/messageparser');
 var l = require('./lib/log');
 
 var app = express();
@@ -9,7 +11,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/toirc', function(req, res){
   l.debug('Got incoming msg to IRC: ', req.body);
-  res.status(200).end();
+  messageparser.parseIrcMessage(req.body)
+  .then(function(message) {
+    l.debug('IRC message parsed');
+    return ircdispatcher.postMessage(message);
+  })
+  .then(function() {
+    l.verbose('IRC message sent to IRC server');
+    res.status(200).end();
+  }, function error (e) {
+    if (e.name !== 'HttpError')
+      l.error('%s\nStack: "%s"', e.toString(), e.stack);
+    res.status(e.statusCode);
+  })
+  .done();
 });
 
 var server = app.listen(config.server.port, function() {
